cf23beb398a82b9c83fe2df364be50b079a4037d
MaelC001 <110736025+mael-charpentier@users.noreply.github.com>
2025-07-10 12:28:54 -0400

    Create params.py

---
+++ params.py
@@ -0,0 +1,14 @@
+all_exercises = [
+    {
+        "name" : "ex1",
+        "path" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.md"
+    },
+    {
+        "name" : "ex2",
+        "path" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex2.md"
+    },
+    {
+        "name" : "ex3",
+        "path" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex3.md"
+    }
+]
