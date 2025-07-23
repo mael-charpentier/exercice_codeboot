5a982239aab62cdd025215207204f59540b22875
MaelC001 <110736025+mael-charpentier@users.noreply.github.com>
2025-07-23 15:01:45 -0400

    add page required

--- params.py
+++ params.py
@@ -12,21 +12,31 @@ random_exercise = 2
 
 all_exercises = [
     {
+        "name" : "intro",
+        "path_content" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex3.md",
+        #"path_code" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.py",
+        "type_page" : "codeboot_without",
+        "require" : "True"
+    },
+    {
         "name" : "ex1",
         "path_content" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.md",
         #"path_code" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.py",
-        "type_page" : "codeboot_fix" # "codeboot_fix", "codeboot_flottant", "codeboot_flottant_hidden", "codeboot_without"
+        "type_page" : "codeboot_fix", # "codeboot_fix", "codeboot_flottant", "codeboot_flottant_hidden", "codeboot_without"
+        "require" : "False"
     },
     {
         "name" : "ex2",
         "path_content" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex2.md",
         #"path_code" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.py",
-        "type_page" : "codeboot_fix"
+        "type_page" : "codeboot_fix",
+        "require" : "False"
     },
     {
-        "name" : "ex3",
+        "name" : "ex4",
         "path_content" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex3.md",
         #"path_code" : "https://raw.githubusercontent.com/mael-charpentier/exercice_codeboot/refs/heads/codeboot/ex1.py",
-        "type_page" : "codeboot_without"
+        "type_page" : "codeboot_fix",
+        "require" : "False"
     }
 ]
